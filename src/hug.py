9e25343ec6c7a3155aee7059b36e0b9fff04f2cc
shaithegame <127069537+shaithegame@users.noreply.github.com>
2023-08-08 02:33:16 +0000

    Hug command fix

--- src/hug.py
+++ src/hug.py
@@ -2,5 +2,5 @@ import discord
 from src.client import client
 
 @client.hybrid_command(description='Pings a selected user and gives them a hug')
-async def hug(ctx, (user1: discord.User, required=True), (user2: discord.User, required=False)):
+async def hug(ctx, user1: discord.User, required=True, Option(user2: discord.User, required=False)):
   await ctx.send(f'Hugs for {user1.mention} and {user2.mention} from {ctx.author.mention}! =(^w^)=')
