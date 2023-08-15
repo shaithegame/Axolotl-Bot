bb94425e8afb21b2e5d8c4f8748050156cb428f0
shaithegame <127069537+shaithegame@users.noreply.github.com>
2023-08-15 22:04:55 +0000

    Ask test fix 2

--- src/ask.py
+++ src/ask.py
@@ -2,9 +2,10 @@ import discord
 from src.client import client
 
 @client.hybrid_command
-async def ask(ctx):
-  await ctx.send('Question time initiated, ask away!')
+async def ask(ctx, *, smg: str):
+  await ctx.send('Question time ready!')
+  
   message=await client.wait_for('message')
-
+  
   if 'pronoun' or 'pronounce' in msg.content_lower():
-    await ctx.send('test')
+    await message.send('test')
